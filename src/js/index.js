5732463a7a04b58b534f153a862722458eb12496
Claudio5957 <123334255+Claudio5957@users.noreply.github.com>
2023-01-22 19:21:15 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+console.log('mostar o socument', document);
+
+console.log(document.querySelector(".botao-trailer"));
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList. toggle("aberto");}
+
+botaoTrailer.addEventListener("click", () => {       
+    alternarModal();
+video.setAttribute("src", linkDoVideo)})
+botaoFecharModal.addEventListener("click", ()=>{
+    alternarModal();
+    video.setAttribute("src", "");});
+
+
+
+
+
+
